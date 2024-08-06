aa8e689074ebf61d509727fa82827ae1be097b3d
Javier <javier.rostagno@gmail.com>
2024-08-06 15:37:41 -0300

    feat:padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center p-24">
+    <main className="flex flex-col items-center p-30">
       <span className="text-5xl">Hola Mundo !!!!</span>
       <Link href={"/about"}>About Page</Link>
     </main>
